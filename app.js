2ce9ccccb8f4c6fda3a7726fcf26ce2c512026e7
RodolfoBaume <robalaz@hotmail.com>
2022-04-26 15:47:28 -0500

    Agregando endpoint GET /v1/explorers

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const port = 3000
 
 //routes
 
+//ESTO ES UN ENDPOINT
+// HTTP METHODS: GET, POST, PUT, DELETE
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL request ${new Date()}`)
+    const explorer1 = {id: 1, name: "Rbaume1"}
+    const explorer2 = {id: 2, name: "Rbaume2"}
+    const explorer3 = {id: 3, name: "Rbaume3"}
+    const explorer4 = {id: 4, name: "Rbaume4"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
